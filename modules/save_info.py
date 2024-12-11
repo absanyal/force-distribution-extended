b8f0eb8f9efeec87b62189cde26f71d29c1ce16f
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2024-12-11 01:44:07 -0500

    Update save_info.py to include additional parameter d_e in filament info output

--- modules/save_info.py
+++ modules/save_info.py
@@ -2,7 +2,7 @@ import numpy as np
 from modules.filament import filament
 
 def save_filament_info(filament_name: filament, info_file_name: str):
-    R, d, a, a1, a2, l, s1, s2, aF, aL, theta1, theta2, gamma, phi1, phi2, phi3, phi4 = filament_name.get_parameters()
+    R, d, d_e, a, a1, a2, l, s1, s2, aF, aL, theta1, theta2, gamma, phi1, phi2, phi3, phi4 = filament_name.get_parameters()
 
     num_monomers = filament_name.num_monomers
     num_layers = filament_name.num_layers
@@ -12,10 +12,11 @@ def save_filament_info(filament_name: filament, info_file_name: str):
     num_angles = filament_name.num_angles
     
     with open(info_file_name, 'w') as info_file:
-        info_file.write("#R \t d \t a \t a1 \t a2 \t l \t s1 \t s2 \t aF \t aL \t theta1 \t theta2 \t gamma \t phi1 \t phi2 \t phi3 \t phi4 num_monomers \t num_layers \t num_total_particles \t num_linkers \t num_bonds \t num_angles\n")
+        info_file.write("#R \t d \t d_e \t a \t a1 \t a2 \t l \t s1 \t s2 \t aF \t aL \t theta1 \t theta2 \t gamma \t phi1 \t phi2 \t phi3 \t phi4 num_monomers \t num_layers \t num_total_particles \t num_linkers \t num_bonds \t num_angles\n")
         
         info_file.write("{:.4f} \t".format(R))
         info_file.write("{:.4f} \t".format(d))
+        info_file.write("{:.4f} \t".format(d_e))
         info_file.write("{:.4f} \t".format(a))
         info_file.write("{:.4f} \t".format(a1))
         info_file.write("{:.4f} \t".format(a2))
